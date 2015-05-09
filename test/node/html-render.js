e9f5b43ced56eee500f7afdf9323e6c79107de04
pH200 <ph.minamo@cytisan.com>
2015-05-10 06:38:52 +0800

    Complete tests for renderAsHTML #1

--- test/node/html-render.js
+++ test/node/html-render.js
@@ -18,117 +18,64 @@ describe('renderAsHTML()', function () {
     });
   });
 
-  it.skip('should render a simple nested custom element as HTML', function (done) {
+  it('should render a simple nested custom element as HTML', function (done) {
     let MyElement = Cycle.createReactClass('MyElement', function () {
       return Rx.Observable.just(h('h3.myelementclass'));
     });
-    var vtree$ = Rx.Observable.just(
-      h('div.test-element', h(MyElement))
+    let vtree$ = Rx.Observable.just(
+      h('div.test-element', null, h(MyElement))
     );
     let html$ = Cycle.renderAsHTML(vtree$);
     html$.subscribe(function (html) {
-      assert.strictEqual(html,
-        '<div class="test-element">' +
-          '<h3 class="myelementclass"></h3>' +
-        '</div>'
-      );
+      let $ = cheerio.load(html);
+      assert.ok($('*').first().is('div.test-element'));
+      assert.strictEqual($('div.test-element').length, 1);
+      assert.ok($('div.test-element').children().is('h3.myelementclass'));
       done();
     });
   });
 
-  it.skip('should render double nested custom elements as HTML', function (done) {
-    Cycle.registerCustomElement('myelement', function () {
-      return {
-        vtree$: Rx.Observable.just(h('h3.myelementclass'))
-      };
+  it('should render double nested custom elements as HTML', function (done) {
+    let MyElement = Cycle.createReactClass('MyElement', function () {
+      return Rx.Observable.just(h('h3.myelementclass'));
     });
-    Cycle.registerCustomElement('nice-element', function () {
-      return {
-        vtree$: Rx.Observable.just(h('div.a-nice-element', [
-          String('foobar'), h('myelement')
-        ]))
-      };
+    let NiceElement = Cycle.createReactClass('NiceElement', function () {
+      return Rx.Observable.just(h('div.a-nice-element', null, [
+        'foobar', h(MyElement)
+      ]));
     });
-    var vtree$ = Rx.Observable.just(h('div.test-element', [h('nice-element')]));
+    let vtree$ = Rx.Observable.just(h(
+      'div.test-element', null, h(NiceElement)
+    ));
     let html$ = Cycle.renderAsHTML(vtree$);
     html$.subscribe(function (html) {
-      assert.strictEqual(html,
-        '<div class="test-element">' +
-          '<div class="a-nice-element">foobar<h3 class="myelementclass"></h3></div>' +
-        '</div>'
-      );
+      let $ = cheerio.load(html);
+      assert.ok($('*').first().is('div.test-element'));
+      assert.strictEqual($('div.test-element').length, 1);
+      assert.ok($('div.test-element').children().is('div.a-nice-element'));
+      assert.strictEqual($('div.a-nice-element').children().first().text(), 'foobar');
+      assert.ok($('div.a-nice-element').children().eq(1).is('h3.myelementclass'));
       done();
     });
   });
 
-  it.skip('should render a nested custom element with props as HTML', function (done) {
-    Cycle.registerCustomElement('myelement', function (interactions, props) {
-      return {
-        vtree$: props.get('foobar')
-          .map(foobar => h('h3.myelementclass', String(foobar).toUpperCase()))
-      };
+  it('should render a nested custom element with props as HTML', function (done) {
+    let MyElement = Cycle.createReactClass('MyElement', function (_, props) {
+      return props.get('foobar')
+        .map(foobar => h('h3.myelementclass', null, String(foobar).toUpperCase()))
     });
-    var vtree$ = Rx.Observable.just(
+    let vtree$ = Rx.Observable.just(
       h('div.test-element', [
-        h('myelement', {foobar: 'yes'})
+        h(MyElement, {foobar: 'yes'})
       ])
     );
     let html$ = Cycle.renderAsHTML(vtree$);
     html$.subscribe(function (html) {
-      assert.strictEqual(html,
-        '<div class="test-element">' +
-          '<h3 class="myelementclass">YES</h3>' +
-        '</div>'
-      );
-      done();
-    });
-  });
-
-  it.skip('should render a complex custom element tree as HTML', function (done) {
-    Cycle.registerCustomElement('x-foo', function () {
-      return {
-        vtree$: Rx.Observable.just(h('h1.fooclass'))
-      };
-    });
-    Cycle.registerCustomElement('x-bar', function () {
-      return {
-        vtree$: Rx.Observable.just(h('h2.barclass'))
-      };
-    });
-    var vtree$ = Rx.Observable.just(
-      h('.test-element', [
-        h('div', [
-          h('h2.a', 'a'),
-          h('h4.b', 'b'),
-          h('x-foo')
-        ]),
-        h('div', [
-          h('h3.c', 'c'),
-          h('div', [
-            h('p.d', 'd'),
-            h('x-bar')
-          ])
-        ])
-      ])
-    );
-    let html$ = Cycle.renderAsHTML(vtree$);
-    html$.subscribe(function (html) {
-      assert.strictEqual(html,
-        '<div class="test-element">' +
-          '<div>' +
-            '<h2 class="a">a</h2>' +
-            '<h4 class="b">b</h4>' +
-            '<h1 class="fooclass"></h1>' +
-          '</div>' +
-          '<div>' +
-            '<h3 class="c">c</h3>' +
-            '<div>' +
-              '<p class="d">d</p>' +
-              '<h2 class="barclass"></h2>' +
-            '</div>' +
-          '</div>' +
-        '</div>'
-      );
+      let $ = cheerio.load(html);
+      assert.ok($('*').first().is('div.test-element'));
+      assert.strictEqual($('div.test-element').length, 1);
+      assert.ok($('div.test-element').children().is('h3.myelementclass'));
+      assert.strictEqual($('h3.myelementclass').text(), 'YES');
       done();
     });
   });
