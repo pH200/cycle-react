5a79abe1c08fd991310ab2d3ab86a737f99a94c2
pH200 <ph.minamo@cytisan.com>
2019-08-14 17:04:09 +0700

    Update react native example

--- examples/native/hello/App.js
+++ examples/native/hello/App.js
@@ -5,23 +5,28 @@ import {
   TextInput,
   View
 } from 'react-native';
-import {component} from 'cycle-react/rxjs';
-import 'rxjs/Rx';
+import { useInteractions } from 'cycle-react/rxjs';
+import { map } from 'rxjs/operators';
 
-export default component('hello', function computer(interactions) {
-  return interactions.get('OnInputChange')
-    .map((ev) => ev.nativeEvent.text)
-    .startWith('')
-    .map((name) => (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Name:</Text>
-        <TextInput
-              style={{height: 40, width: 120, margin: 10, borderColor: 'gray', borderWidth: 1}}
-              onChange={interactions.listener('OnInputChange')} />
-        <Text style={styles.instructions}>Hello {name}</Text>
-      </View>
-    ));
-});
+const [interactions, useCycle] = useInteractions(
+  '',
+  {
+    onInputChange: map(ev => () => ev.nativeEvent.text)
+  }
+);
+
+export default function Hello() {
+  const name = useCycle();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Name:</Text>
+      <TextInput
+        style={{height: 40, width: 120, margin: 10, borderColor: 'gray', borderWidth: 1}}
+        onChange={interactions('onInputChange')} />
+      <Text style={styles.instructions}>Hello {name}</Text>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
